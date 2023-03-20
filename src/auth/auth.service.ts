aefdd3a35fa55d598596df37f52537c506e87856
Guilherme Maia e Silva <guilherme.maia@creditas.com>
2023-03-20 20:06:47 -0300

    implementing user validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/users.entity';
+import { HashPasswordService } from './hash.password.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private hashPasswordService: HashPasswordService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    // const user = await this.usersService.findOne(username);
-    // if (user && user.password === pass) {
-    //   const { password, ...result } = user;
-    //   return result;
-    // }
-    // return null;
+  async validateUser(username: string, password: string): Promise<any> {
+    const user: User = await this.usersService.findOne(username);
+
+    if (!user) {
+      return null;
+    }
+    const matchPassword = await this.hashPasswordService.comparePassword(
+      password,
+      user.password,
+    );
+
+    if (!matchPassword) return null;
+    return user.username;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+  async login(user: User) {
+    const payload = { username: user.username, sub: user.email };
     return {
       access_token: this.jwtService.sign(payload),
     };
