0bda42276a7377b031c22fb5eebb675a4c17aff5
gms92 <guibushido0206@gmail.com>
2023-03-20 23:51:49 -0300

    implementing auth service unit tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,46 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
-import { JwtModule, JwtService } from '@nestjs/jwt';
 import { UsersRepository } from '../users/users.repository';
-import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { jwtConstants } from './constants';
-import { AuthController } from './auth.controller';
-import { LocalStrategy } from './local.strategy';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersSchema } from '../users/users.schema';
 import { MongoConfig } from '../config/mongo.config';
+import { User } from '../users/users.entity';
+import { AuthModule } from './auth.module';
+import { HashPasswordService } from './hash.password.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
   let app: TestingModule;
+  let authService: AuthService;
+  let usersService: UsersService;
+  let usersRepository: UsersRepository;
 
   beforeEach(async () => {
     app = await Test.createTestingModule({
       imports: [
-        UsersModule,
-        PassportModule,
-        JwtModule.register({
-          secret: jwtConstants.secret,
-          signOptions: { expiresIn: '600s' },
-        }),
+        AuthModule,
         MongooseModule.forRoot(MongoConfig.MONGODB_TEST_URI),
         MongooseModule.forFeature([{ name: 'users', schema: UsersSchema }]),
       ],
-      controllers: [AuthController],
-      providers: [AuthService, LocalStrategy],
-      exports: [AuthService],
     }).compile();
 
-    service = app.get<AuthService>(AuthService);
+    authService = app.get<AuthService>(AuthService);
+    usersService = app.get<UsersService>(UsersService);
+    usersRepository = app.get<UsersRepository>(UsersRepository);
   });
   afterEach(async () => {
-    // await repository.dropCollection();
+    await usersRepository.dropCollection();
     app.close();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('.validateUser', () => {
+    it('validates a user if it exists in the database', async () => {
+      const user: User = {
+        username: 'teste',
+        password: HashPasswordService.hashPassword('teste12345'),
+        email: 'teste@teste.com',
+      };
+      await usersService.create(user);
+
+      const result: User | null = await authService.validateUser(
+        user.username,
+        'teste12345',
+      );
+
+      expect(result).toBe(user.username);
+    });
+    
+    it('invalidates a user if it not exists in the database', async () => {
+      const user: User = {
+        username: 'teste',
+        password: HashPasswordService.hashPassword('teste12345'),
+        email: 'teste@teste.com',
+      };
+
+      const result: User | null = await authService.validateUser(
+        user.username,
+        'teste12345',
+      );
+
+      expect(result).toBe(null);
+    });
   });
 });
